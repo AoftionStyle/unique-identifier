9463e7fba0907c56176aa260b45d9c3ada2699db
AOFTION <chutiponpon@gosoft.oc.th>
2022-12-01 21:22:11 +0700

    removed @ToString

--- src/main/java/com/aoftion/unique/identifier/util/cryptography/cipher/configuration/CipherCryptoAlgorithmConfiguration.java
+++ src/main/java/com/aoftion/unique/identifier/util/cryptography/cipher/configuration/CipherCryptoAlgorithmConfiguration.java
@@ -2,11 +2,9 @@ package com.aoftion.unique.identifier.util.cryptography.cipher.configuration;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 public class CipherCryptoAlgorithmConfiguration {
     private String factory;
     private String spec;
